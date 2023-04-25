8fb78f09d57c388f4502f6d482d8ec283de7bec7
salman-rafii <salmanrafi125@gmail.com>
2023-04-26 00:07:52 +0500

    user state persistance done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:zoom_clone_flutter/resources/auth_methods.dart';
 import 'package:zoom_clone_flutter/screens/home_screen.dart';
 import 'package:zoom_clone_flutter/screens/login_screen.dart';
 import 'package:zoom_clone_flutter/utils/colors.dart';
@@ -20,7 +21,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
       ),
-      home: StreamBuilder(builder: (context, snapshot) => const Text("")),
+      home: StreamBuilder(
+        stream: AuthMethods().authChanges,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          }
+          return const LoginScreen();
+        },
+      ),
       routes: {
         "/login": (context) => const LoginScreen(),
         "/home-screen": (context) => const HomeScreen(),
